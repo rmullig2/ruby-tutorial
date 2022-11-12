003fcb8a04f817cba861915ae8df2bbc01c873d2
Ray Mulligan <rmullig2@optonline.net>
2022-11-12 17:30:46 -0500

    further changes to config files

--- scripts/prelims.sh
+++ scripts/prelims.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 mkdir /home/ec2-user/tutorial
 cd /home/ec2-user/tutorial
-rbenv local 3.1.2
-bundle install
+/home/ec2-user/.rbenv/bin/rbenv local 3.1.2
+/home/ec2-user/.rbenv/shims/bundle install
